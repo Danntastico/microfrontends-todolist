4b1e26c7f7ccfefad0d7e90286bdec424476f105
daniloWindows <nicolasdpf@gmail.com>
2021-01-20 15:43:03 -0500

    Created the component Headbar

---
+++ headbar/src/index.js
@@ -0,0 +1,12 @@
+const Header = `
+    <nav>
+        <h1>Hola Mundo!</h1>
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+        </ul>
+    </nav>
+`;
+
+document.querySelector("#headerBar").innerHTML = Header;
